aa5c35c24f3130dd7f59cb447afd705efc66606e
Saroj Shakya <sshakya@kathmandu>
2019-11-14 16:32:08 +0545

    added new files

---
+++ Master/Master.c
@@ -0,0 +1,104 @@
+/*
+ * SPI_Interface.c
+ *
+ * Created: 7/29/2014 8:01:11 AM
+ *  Author: Saroj Shakya
+ */ 
+
+
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#define MOSI 5
+#define SCK 7
+#define LCD_DATA_PORT PORTA
+#define LCD_DATA_DDR DDRA
+#define LCD_RS 2
+#define LCD_EN 3
+//LCD Code starts from here//
+void LCD_cmnd(unsigned char cmnd) //function to send command to LCD Module
+{
+	LCD_DATA_PORT = (LCD_DATA_PORT & 0x0F) | (cmnd & 0xF0); //send upper 4 bit
+	LCD_DATA_PORT &= ~(1<<LCD_RS); //0b11111011 //RS = 0
+	//LCD_DATA_PORT &= ~(1<<LCD_RW); //0b11111011//RW = 0
+	LCD_DATA_PORT |= 1<<LCD_EN; //0b00001000 //EN = 1
+	_delay_us(50);
+	LCD_DATA_PORT &= ~(1<<LCD_EN); //0b11110111 //EN = 0
+	_delay_us(200);
+	LCD_DATA_PORT = (LCD_DATA_PORT & 0x0F) | (cmnd << 4); //send lower 4 bit
+	LCD_DATA_PORT |= 1<<LCD_EN; //0b00001000 //EN = 1
+	_delay_us(50);
+	LCD_DATA_PORT &= ~(1<<LCD_EN); //0b11110111 //EN = 0
+}
+void LCD_data(unsigned char data) //Function to send data to LCD Module
+{
+	LCD_DATA_PORT = (LCD_DATA_PORT & 0x0F) | (data & 0xF0);
+	LCD_DATA_PORT |= 1<<LCD_RS; //0b00000100 //RS = 1
+	//LCD_DATA_PORT &= ~(1<<LCD_RW); //0b11111011
+	LCD_DATA_PORT |= 1<<LCD_EN; //0b00001000
+	_delay_us(50);
+	LCD_DATA_PORT &= ~(1<<LCD_EN); //0b11110111
+	_delay_us(2000);
+	LCD_DATA_PORT = (LCD_DATA_PORT & 0x0F) | (data << 4);
+	LCD_DATA_PORT |= 1<<LCD_EN; //0b00001000
+	_delay_us(2000);
+	LCD_DATA_PORT &= ~(1<<LCD_EN); //0b11110111
+}
+void LCD_initialize(void) //Function to initialize LCD Module
+{
+	LCD_DATA_DDR = 0xFC;
+	LCD_DATA_PORT &= ~(1<<LCD_EN); //0b11110111;
+	_delay_ms(200);
+	LCD_cmnd(0x33);
+	_delay_ms(20);
+	LCD_cmnd(0x32);
+	_delay_ms(20);
+	LCD_cmnd(0x28);
+	_delay_ms(20);
+	LCD_cmnd(0x0C);
+	_delay_ms(20);
+	LCD_cmnd(0x01);
+	_delay_ms(20);
+}
+void LCD_clear(void)
+{
+	LCD_cmnd(0x01);
+	_delay_ms(2);
+}
+void LCD_print(char * str)
+{
+	unsigned char i=0;
+	while(str[i] != 0)
+	{
+		LCD_data(str[i]);
+		i++;
+		_delay_ms(5);
+	}
+}
+void LCD_set_curser(unsigned char y, unsigned char x)
+{
+	if(y==1)
+	LCD_cmnd(0x7F+x);
+	else if(y==2)
+	LCD_cmnd(0xBF+x);
+}
+void LCD_num(unsigned char num)
+{
+	//LCD_data(num/100 + 0x30);
+	//num = num%100;
+	LCD_data(num/10 + 0x30);
+	LCD_data(num%10 + 0x30);
+}
+int main(void)
+{
+	LCD_initialize();
+	DDRB = (1<<MOSI) | (1<<SCK);
+	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR0);
+    while(1)
+    {
+		SPDR = 'A';
+		while (!(SPSR & (1<SPIF)));
+		LCD_data(SPDR);
+		_delay_ms(200);        
+    }
+}
